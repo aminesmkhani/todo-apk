998279ee84c86a86a8566d6f34b13555e717b3d2
Aмιη <amin.esmkhani75@gmail.com>
2024-08-24 10:27:00 +0330

    add hive type for enums!

--- lib/data.dart
+++ lib/data.dart
@@ -1,13 +1,22 @@
 import 'package:hive_flutter/adapters.dart';
 part 'data.g.dart';
+
 @HiveType(typeId: 0)
-class Task extends HiveObject{
+class Task extends HiveObject {
   @HiveField(0)
-  String name='';
+  String name = '';
   @HiveField(1)
-  bool isCompleted=false;
+  bool isCompleted = false;
   @HiveField(2)
-  Priority priority=Priority.low;
+  Priority priority = Priority.low;
 }
 
-enum Priority { low, normal, hight }
+@HiveType(typeId: 1)
+enum Priority {
+  @HiveField(0)
+  low,
+  @HiveField(1)
+  normal,
+  @HiveField(2)
+  hight
+}
