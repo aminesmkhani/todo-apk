1531cc904812311a8eafc24f47622bca7fcab203
Aмιη <amin.esmkhani75@gmail.com>
2024-09-03 18:14:21 +0330

    implement Repository with source or Provider!

--- lib/data/repo/repository.dart
+++ lib/data/repo/repository.dart
@@ -1,3 +1,37 @@
-class Repository<T>{
-  
+import 'package:todolist/data/source/source.dart';
+
+class Repository<T> implements DataSource<T> {
+  final DataSource<T> localDataSource;
+
+  Repository({required this.localDataSource});
+
+  @override
+  Future<T> createOrUpdate(T data) {
+    return localDataSource.createOrUpdate(data);
+  }
+
+  @override
+  Future<void> delete(T data) {
+    return localDataSource.delete(data);
+  }
+
+  @override
+  Future<void> deleteAll() {
+    return deleteAll();
+  }
+
+  @override
+  Future<void> deleteById(id) {
+    return deleteById(id);
+  }
+
+  @override
+  Future<T> findById(id) {
+    return findById(id);
+  }
+
+  @override
+  Future<List<T>> getAll({String searchKeyword=''}) {
+    return getAll(searchKeyword: searchKeyword);
+  }
 }
